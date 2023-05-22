9a0071a57401820ac3cb969c307fcee2106e6dd2
1234 <a1234@johnWood.local>
2023-05-22 14:27:44 +0300

    fixed base url with https in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http://johny-wood.github.io/winoptiongame",
+  base: "https://johny-wood.github.io/winoptiongame",
 });
