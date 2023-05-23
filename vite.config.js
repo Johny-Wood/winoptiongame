89cdae9bdd45a8e61aef64c862c1ea91b8d6011f
1234 <a1234@johnWood.local>
2023-05-23 13:28:28 +0300

    added json rollup plugin for changing paths in json moc files

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import json from "@rollup/plugin-json";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), json()],
   base: "https://johny-wood.github.io/winoptiongame",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@json": path.resolve(__dirname, "/mock"),
     },
   },
 });
