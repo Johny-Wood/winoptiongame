13e4d34ef2284d32023d6651dbd69d558c52b760
1234 <a1234@johnWood.local>
2023-05-23 17:58:43 +0300

    trying to fix paths in jsons

--- src/api/handlers/getUser/index.js
+++ src/api/handlers/getUser/index.js
@@ -1,7 +1,7 @@
 import axiosClient from "../../index.js";
 
 const getUser = () => {
-  return axiosClient.get("/src/mock/user.json");
+  return axiosClient.get("/mock/user.json");
 };
 
 export default getUser;
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import replace from "rollup-plugin-replace";
-import json from "@rollup/plugin-json";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    json(),
-    replace({
-      '"/src/assets/"': '"/assets/"',
-    }),
-  ],
+  plugins: [react()],
   base: "https://johny-wood.github.io/winoptiongame",
   resolve: {
     alias: {
